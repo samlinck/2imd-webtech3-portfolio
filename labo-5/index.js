a6ef3cc96fa96e6b6e7b6d2529c95b53ccba4bb9
Sam <samlinck@outlook.com>
2019-04-02 19:32:19 +0200

    set up the app structure

--- labo-5/index.js
+++ labo-5/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const messageRouter = require("./routers/message");
 
 app.get('/', (req, res) => res.send('Hello World!'));
+app.use('/api/v1/', messageRouter);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
