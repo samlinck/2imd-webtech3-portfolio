0df391c580dc9c97d8facca5742f2f36affb941a
Sam <samlinck@outlook.com>
2019-04-02 20:55:52 +0200

    all messages are operational

--- labo-5/controllers/message.js
+++ labo-5/controllers/message.js
@@ -8,17 +8,55 @@ let get = (req, res) => {
 let post = (req, res) => {
     res.json({
         "status": "success",
-        "message": "POSTING a new message for user Pickachu"
+        "message": "POSTING a new message for user Bojack"
     });
 }
 
-let id = (req, res) => {
+let getOne = (req, res) => {
+    let id = req.params.id;
     res.json({
         "status": "success",
-        "message": "GETTING message with ID 21"
+        "message": {
+            "id": id,
+            "message": "GETTING message with ID " + id
+    }
+        
+    });
+}
+
+let forUser = (req, res) => {
+    res.json({
+        "status": "success",
+        "message": "GETTING messages for user bojack"
+    });
+}
+
+let del = (req, res) => {
+    let id = req.params.id;
+    res.json({
+        "status": "success",
+        "message": {
+            "id": id,
+            "message": "DELETING message with ID " + id
+        }
+    });
+}
+
+let put = (req, res) => {
+    let id = req.params.id;
+    res.json({
+        "status": "success",
+        "message": {
+            "id": id,
+            "message": "UPDATING message with ID" + id
+        }
     });
 }
 
 module.exports.get = get;
 module.exports.post = post;
-module.exports.get = id;
+module.exports.del = del;
+module.exports.put = put;
+
+module.exports.getOne = getOne;
+module.exports.forUser = forUser;
--- labo-5/routers/message.js
+++ labo-5/routers/message.js
@@ -3,7 +3,11 @@ const router = express.Router();
 const messageController = require('../controllers/message');
 
 router.get('/messages', messageController.get);
+router.get('/messages/:id', messageController.getOne);
+// router.get('/messages?user=bojack', messageController.forUser);
 router.post('/messages', messageController.post);
-router.get('/messages/:id', messageController.get);
+router.put('/messages/:id', messageController.put);
+router.delete('/messages/:id', messageController.del);
+
 
 module.exports = router;
